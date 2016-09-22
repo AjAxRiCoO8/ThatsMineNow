6ae34224429ee0eb8f3c05874cb4b1b55dabd6c4
Nino Thijs <ninothijs@gmail.com>
2016-09-22 14:10:47 +0200

    gun
    mogelijke gun

---
+++ ThatsMineNow/Assets/ThatsMineNow/nino/scripts/PressToPickUp.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PressToPickUp : MonoBehaviour
+{
+
+	[Range (0.5f, 10f)]
+	public float viewingDistance;
+	public Text interactionText;
+
+	public int treasureCount = 0;
+
+	RaycastHit hit;
+	float theDistance;
+	Vector3 forward;
+
+	// Use this for initialization
+	void Update ()
+	{
+		forward = transform.TransformDirection (Vector3.forward) * viewingDistance;
+		Debug.DrawRay (transform.position, forward, Color.green);
+
+		if (Physics.Raycast (transform.position, forward, out hit)) {
+			//theDistance = hit.distance;
+			//print (theDistance + " " + hit.collider.gameObject.tag);
+
+			if (hit.collider.gameObject.CompareTag ("PickUp")) {
+				interactionText.text = "Press 'E' to pick up";
+				if (Input.GetKeyDown (KeyCode.E)) {
+					hit.collider.gameObject.SetActive (false);
+					treasureCount++;
+					interactionText.text = "";
+				}
+			} else {
+				interactionText.text = "";
+			}
+		}
+	}
+}
---
+++ ThatsMineNow/Assets/ThatsMineNow/nino/scripts/Shoot.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+    
+    public GameObject bullet;
+    AudioSource audio;
+
+
+
+    // Use this for initialization
+    void Start () {
+        audio = GetComponent<AudioSource>();
+        
+        
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	    if(Input.GetButtonDown("Fire1"))
+        {
+            audio.Play();
+            Instantiate(bullet, transform.position, transform.rotation);
+            
+            
+
+           
+        }
+
+	}
+}
