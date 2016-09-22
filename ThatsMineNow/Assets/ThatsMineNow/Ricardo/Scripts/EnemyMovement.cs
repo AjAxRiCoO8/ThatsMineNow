6fd96a179360ab4c6b3342cee55f868c32e55933
Ricardo Desmedt <Ricardo-42-ajax@live.nl>
2016-09-22 12:15:02 +0200

    Enemy with some AI stuff

---
+++ ThatsMineNow/Assets/ThatsMineNow/Ricardo/Scripts/EnemyMovement.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+
+	public GameObject ball;
+	public GameObject gate;
+	public float speed = 10000.0f;
+
+	public bool walkToBall = true;
+
+	bool reachedBall = false;
+	bool ballIsChild = false;
+	bool isHit = false;
+
+	Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+		transform.eulerAngles = new Vector3 (0, transform.eulerAngles.y, 0);
+	}
+
+	void Update() {
+
+		Debug.Log (transform.eulerAngles);
+		Debug.Log (IsStanding ());
+		if (walkToBall && !reachedBall && !isHit) {
+			WalkToObject (ball);
+		} else {
+			Freeze ();
+		}
+
+		if (reachedBall && !ballIsChild) {
+			ball.transform.parent = transform;
+			ball.transform.localPosition = new Vector3(-0.61f, -0.057f, 0.4f);
+			ballIsChild = true;
+		}
+
+		if (ballIsChild) {
+			WalkToObject (gate);
+		}
+
+		if (!IsStanding() && isHit && rb.velocity.Equals(Vector3.zero)) {
+			StandUp ();
+		}
+
+	}
+
+	public void WalkToObject(GameObject gameObject) {
+		Vector3 objectPosition = gameObject.transform.position;
+		Vector3 pathVector = objectPosition - transform.position;
+		pathVector.y = 0;
+
+		rb.velocity = new Vector3 (0, 0, 0);
+
+		Vector3 normalizedPathVector = pathVector.normalized;
+
+		if (pathVector.sqrMagnitude > 5) {
+			rb.AddForce (normalizedPathVector * (speed * Time.deltaTime));
+			transform.LookAt (objectPosition);
+			transform.eulerAngles = new Vector3 (0, transform.eulerAngles.y, 0);
+			reachedBall = false;
+		} else {
+			rb.velocity = new Vector3 (0, 0, 0);
+			reachedBall = true;
+		}
+	}
+
+	public void Freeze() {
+		rb.velocity = new Vector3 (0, 0, 0);
+	}
+
+	public void Hit() {
+		isHit = true;
+	}
+
+	bool IsStanding() {
+		return transform.eulerAngles.x == 0 && transform.eulerAngles.z == 0;
+	}
+
+	void StandUp() {
+		transform.eulerAngles = new Vector3 (0, 0, 0);
+	}
+
+}
